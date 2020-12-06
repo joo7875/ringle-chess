eca001ed0f3a0cbecf22ec16f8a8b31b1bde6f02
Hyewon Joo <joo7875@gmail.com>
2020-12-06 11:02:27 +0900

    Added pawn input

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ const size = 70;
 class App extends Component {
 
   state = {
-    boardInput: null,
-    pawnInput: null
+    boardInput: 0,
+    pawnInput: 0
   }
 
 
@@ -20,9 +20,58 @@ class App extends Component {
     }
   }
 
+  onAddPawn = (board, pawn) => {
 
-  onDrawBoard = (board, pawn) => {
+    var pawnDiv = document.getElementById('pawnDiv');
 
+    // remove all child before drawing new pawns
+    while (pawnDiv.hasChildNodes()) {
+      pawnDiv.removeChild(pawnDiv.lastChild);
+    }
+
+    var pawnDesc = document.createElement('span');
+    pawnDesc.setAttribute('class', 'title');
+    pawnDesc.innerText = 'Location (x, y)';
+    pawnDesc.setAttribute('style', 'margin-right: 10px');
+
+    pawnDiv.appendChild(pawnDesc);
+    
+    for (var i = 0; i < pawn; i++) {
+      var spanOpen = document.createElement('span');
+      spanOpen.innerText = '[';
+
+      var comma = document.createElement('span');
+      comma.innerText = ', ';
+
+      var spanClose = document.createElement('span');
+      spanClose.innerText = '] ';
+
+      var pawnInput1 = document.createElement('input');
+      pawnInput1.setAttribute('type', 'number');
+      pawnInput1.setAttribute('class', 'pawnInput');
+      pawnInput1.setAttribute('id', 'pawnInput-' + i + '-0');
+
+      var pawnInput2 = document.createElement('input');
+      pawnInput2.setAttribute('type', 'number');
+      pawnInput2.setAttribute('class', 'pawnInput');
+      pawnInput2.setAttribute('id', 'pawnInput-' + i + '-1');
+
+      pawnDiv.appendChild(spanOpen);
+      pawnDiv.appendChild(pawnInput1);
+      pawnDiv.appendChild(comma);
+      pawnDiv.appendChild(pawnInput2);
+      pawnDiv.appendChild(spanClose);
+    }
+
+    var pawnBtn = document.createElement('button');
+    pawnBtn.setAttribute('class', 'ui button pawnBtn');
+    pawnBtn.innerText = 'Make board';
+
+    pawnDiv.appendChild(pawnBtn);
+  }
+
+
+  onDrawBoard = (board) => {
     var canvas = document.getElementById("board");
     var ctx = canvas.getContext("2d");
 
@@ -62,8 +111,10 @@ class App extends Component {
           <input type='number' className='input marginRight' onChange={this.onInputChange.bind(this, 'board')} />
           <span className='title'>Number of pawn</span>
           <input type='number' className='input marginRight' onChange={this.onInputChange.bind(this, 'pawn')} />
-          <button className="ui button" onClick={() => this.onDrawBoard(boardInput, pawnInput)}>Enter</button>
+          <button className="ui button" onClick={() => this.onAddPawn(boardInput, pawnInput)}>Enter</button>
         </div>
+
+        <div id='pawnDiv'></div>
 
         <canvas id="board" />
       </div>
